27794a19f1450d1f668c58b399ca752e6e085257
wangyangyang <wangyangyang@jieyisoft.com>
2022-10-31 17:40:20 +0800

    项目初始化

---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useMouse } from './../js/mouse.js'
+
+const { x, y } = useMouse()
+</script>
+
+<template>Mouse position is at: {{ x }}, {{ y }}</template>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
