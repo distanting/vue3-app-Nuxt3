f01176e61b672505007f2b0a8ac755979e94955b
wangyangyang <wangyangyang@jieyisoft.com>
2022-11-01 21:32:27 +0800

    init项目

---
+++ nuxt-app/components/Banner/Child.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    轮播图子组件
+  </div>
+</template>
+
+<style lang='less' scoped>
+
+</style>
---
+++ nuxt-app/pages/about/personal.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    个人信息
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: red;
+}
+</style>
